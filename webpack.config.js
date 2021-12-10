17eca7b288d181863f62d36e0e1ba9996d63240e
yoshi <yoshinic@icloud.com>
2021-12-11 01:11:04 +0900

    追加：webpackでHTML,CSSを扱うための設定 ・HTML：html-loader, html-webpack-plugin ・CSS：css-loader, mini-css-extract-plugin, node-sass, sass-loader ・ファイル：globule

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,53 @@
 const path = require("path");
 
-module.exports = {
+// CSS
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+// ファイル名取得
+const globule = require("globule");
+
+// webpackでHTMLを扱う
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// 「dist」ディレクトリパス
+const dist = path.resolve(__dirname, "dist");
+
+const buildDefault = {
   // モード値を production に設定すると最適化された状態で、
   // development に設定するとソースマップ有効でJSファイルが出力される
   mode: "development",
+
   devtool: "inline-source-map",
+
   // メインとなるJavaScriptファイル（エントリーポイント）
-  entry: "./src/main.ts",
+  entry: "./src/index.ts",
 
   module: {
     rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { url: false },
+          },
+          "sass-loader",
+        ],
+      },
       {
         // 拡張子 .ts の場合
         test: /\.ts$/,
         // TypeScript をコンパイルする
         use: "ts-loader",
+      },
+      {
+        // 拡張子 html
+        test: /\.(html)$/,
+        use: {
+          loader: "html-loader",
+        },
       },
     ],
   },
@@ -28,6 +61,34 @@ module.exports = {
   },
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: dist,
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+  ],
 };
+
+const htmlFiles = globule.find("public/*.html");
+// htmlFiles配下にある「.html」ファイルを
+htmlFiles.forEach((htmlsrc) => {
+  // ファイル名を取得 src/html/index.html → index.html
+  const htmlname = htmlsrc.split("/").slice(-1)[0];
+  // webpackの設定にある、pluginsに以下のプラグインインスタンスを入れる。
+  buildDefault.plugins.push(
+    new HtmlWebpackPlugin({
+      // distのファイル名。
+      filename: `${dist}/${htmlname}`,
+      // バンドル対象のjs(main.js)とcss(style.css)を、自動的にトランスパイル後のindex.html入れる。
+      // この場合、トランスパイル前のindex.htmlにjs, cssを入れる必要はない
+      inject: "body",
+      // 対象のhtmlファイル
+      template: htmlsrc,
+      // 圧縮するかどうか。defaultはtrue
+      minify: false,
+    })
+  );
+});
+
+module.exports = buildDefault;
