db79ffe4f6e465273cf1075fee1480995ede57f3
yoshi <yoshinic@icloud.com>
2021-12-11 21:04:00 +0900

    追加：円表示 修正：タイトル

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello World!!");
+import "./sass/style.scss";
+import p5 from "p5";
+
+//JS
+// function setup() {
+//   createCanvas(400, 400);
+// }
+
+// function draw() {
+//   background(220);
+//   ellipse(50, 50, 80, 80);
+// }
+
+// TypeScript
+const sketch = (p: p5) => {
+  p.setup = () => {
+    p.createCanvas(400, 400);
+  };
+
+  p.draw = () => {
+    p.background(220);
+    p.ellipse(50, 50, 80, 80);
+  };
+};
+
+new p5(sketch);
